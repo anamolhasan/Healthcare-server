56d719615ae8f8f1884abd0ac778a29be7e893e2
anamolhasan <mdanamolhasan1998@gmail.com>
2026-02-28 15:14:33 +0600

    auth system added

---
+++ src/app/module/auth/auth.service.ts
@@ -0,0 +1,67 @@
+import { auth } from "../../lib/auth";
+import { UserStatus } from "../../../generated/prisma/enums";
+
+
+
+interface IRegisterPatientPayload {
+    name:string;
+    email:string;
+    password:string;
+}
+
+const registerPatient = async (payload:IRegisterPatientPayload) => {
+    const {name, email, password} = payload
+
+    const data = await auth.api.signUpEmail({
+        body:{
+            name,
+            email,
+            password,
+            // default value
+            // needPasswordChange:false,
+            // role:Role.PATIENT
+        }
+    })
+
+    if(!data.user){
+        throw new Error('Failed to register patient')
+    }
+    // TODO: Create Patient Profile In Transaction After Sign Up of Patient In User Model
+    // const patient = await prisma.$transaction(async(tx)=>{
+    //     await tx.pa
+    // })
+
+    return data
+}
+
+
+interface ILoginUserPayload {
+    email:string;
+    password:string;
+}
+
+const loginUser = async (payload:ILoginUserPayload) => {
+    const {email, password} = payload;
+
+    const data = await auth.api.signInEmail({
+        body:{
+            email,
+            password
+        }
+    })
+
+    if(data.user.status === UserStatus.BLOCKED){
+        throw new Error('User is blocked')
+    }
+
+    if(data.user.isDeleted || data.user.status === UserStatus.DELETED){
+        throw new Error('User is deleted')
+    }
+
+    return data
+}
+
+export const AuthService = {
+    registerPatient,
+    loginUser,
+}
