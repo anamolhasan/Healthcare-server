82b07fd6f1f8bc44ccb0b694ba50e79a782011c9
anamolhasan <mdanamolhasan1998@gmail.com>
2026-03-01 19:53:13 +0600

    get all doctor fetch

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { SpecialtyRoutes } from "../module/specialty/specialty.route";
 import { AuthRoutes } from "../module/auth/auth.route";
 import { UserRouters } from "../module/user/user.route";
+import { DoctorRoutes } from "../module/doctor/doctor.route";
 
 
 const router = Router()
@@ -9,5 +10,9 @@ const router = Router()
 router.use('/auth', AuthRoutes)
 router.use('/specialties', SpecialtyRoutes)
 router.use('/users', UserRouters)
+router.use('/doctors', DoctorRoutes)
+
+
+
 
 export const IndexRoutes = router
