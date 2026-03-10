a9d10eb3544ede00c52541bc98f58adb94b8290f
anamolhasan <mdanamolhasan1998@gmail.com>
2026-03-10 21:47:01 +0600

    auth.ts file update

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -4,6 +4,7 @@ import { prisma } from "./prisma";
 import { Role, UserStatus } from "../../generated/prisma/enums";
 import { envVars } from "../config/env";
 import { bearer, emailOTP } from "better-auth/plugins";
+import { sendEmail } from "../utils/email";
 
 export const auth = betterAuth({
     baseURL:envVars.BETTER_AUTH_URL,
@@ -79,19 +80,48 @@ export const auth = betterAuth({
         emailOTP({
             overrideDefaultEmailVerification:true,
             async sendVerificationOTP({email, otp, type}){
-                if(type === 'email-verification'){
+                if(type ===  "email-verification"){
                     const user = await prisma.user.findUnique({
                         where:{
                             email,
                         }
                     })
 
-                    if(user && !user.emailVerified){
-                        sendEmail 
+                        if(user && !user.emailVerified){
+                            sendEmail({
+                            to:email,
+                            subject:'Verify your email',
+                            templateName:'otp',
+                            templateData:{
+                                name:user.name,
+                                otp,
+                            }
+                        }
+                    )
+                        } 
+                    } else if(type === "forget-password"){
+                        const user = await prisma.user.findUnique({
+                            where:{
+                                email,
+                            }
+                        })
+
+                        if(user){
+                            sendEmail({
+                                to:email,
+                                subject:'Password Reset OTP',
+                                templateName:"otp",
+                                templateData:{
+                                    name:user.name,
+                                    otp,
+                                }
+                            })
+                        }
                     }
                 }
-            }
-        })
+            
+        }),
+ 
     ],
 
     session:{
@@ -101,5 +131,34 @@ export const auth = betterAuth({
             enabled: true,
             maxAge: 60 * 60 * 60 * 24, // 1 day in seconds
         } 
+    },
+
+    redirectURLs:{
+        signIn: `${envVars.BETTER_AUTH_URL}/api/v1/auth/google/success`,
+    },
+
+    trustedOrigins: [process.env.BETTER_AUTH_URL || 'http://localhost:5000', envVars.FRONTEND_URL],
+
+    advanced:{
+        // disableCSRFCheck:true
+        useSecureCookies:false,
+        cookies:{
+            state:{
+                attributes:{
+                    sameSite:"none",
+                    secure:true,
+                    httpOnly:true,
+                    path:'/',
+                }
+            },
+            sessionToken:{
+                attributes:{
+                    sameSite:"none",
+                    secure:true,
+                    httpOnly:true,
+                    path:"/"
+                }
+            }
+        }
     }
 });
--- src/app/utils/email.ts
+++ src/app/utils/email.ts
@@ -4,6 +4,7 @@ import { envVars } from '../config/env'
 import AppError from '../errorHelpers/AppError';
 import status from 'http-status';
 import path from 'path';
+import ejs from 'ejs'
 
 
 const transporter = nodemailer.createTransport({
@@ -30,7 +31,24 @@ interface SendEmailOptions {
 
 export const sendEmail = async ({subject, templateData, templateName, to, attachments} : SendEmailOptions) => {
     try {
-        const templatePath = path
+        const templatePath = path.resolve(process.cwd(), `src/app/templates/${templateName}.ejs`);
+
+        const html = await ejs.renderFile(templatePath, templateData);
+
+        const info = await transporter.sendMail({
+            from : envVars.EMAIL_SENDER.SMTP_FROM,
+            to: to,
+            subject: subject,
+            html: html,
+            attachments: attachments?.map((attachment) => ({
+                filename: attachment.filename,
+                content: attachment.content,
+                contentType: attachment.contentType,
+            }))
+        })
+
+        console.log(`Email send to ${to} : ${info.messageId}`)
+
     } catch (error: any) {
         console.log('Email sending Error', error.message);
         throw new AppError(status.INTERNAL_SERVER_ERROR, 'Failed to send email')
